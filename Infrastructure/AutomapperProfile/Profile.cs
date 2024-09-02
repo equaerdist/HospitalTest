4f5b741c5a01a62351d70da4a2121752ed9f3dea
riyaz <vriyazzz1001@gmail.com>
2024-09-02 21:59:48 +0300

    добавление метода получения докторов по страницам

--- Infrastructure/AutomapperProfile/Profile.cs
+++ Infrastructure/AutomapperProfile/Profile.cs
@@ -10,5 +10,9 @@ public class HospitalProfile : Profile
         CreateMap<AddDoctorDto, Doctor>();
         CreateMap<UpdateDoctorDto, Doctor>()
             .ForMember(x => x.Id, cfg => cfg.Ignore());
+        CreateMap<Doctor, GetDoctorDto>()
+            .ForMember(x => x.CabinetNumber, cfg => cfg.MapFrom(x => x.Cabinet!.CabinetNumber))
+            .ForMember(x => x.SpecializationName, cfg => cfg.MapFrom(x => x.Specialization!.Name))
+            .ForMember(x => x.AreaName, cfg => cfg.MapFrom(x => x.Area != null ? x.Area.AreaNumber : null));
     }
 }
