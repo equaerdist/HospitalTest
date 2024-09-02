9b40fce28132da31caa7056ed13b5bb597c37c20
riyaz <vriyazzz1001@gmail.com>
2024-09-02 21:59:48 +0300

    добавление метода получения докторов по страницам

--- Controllers/DoctorController.cs
+++ Controllers/DoctorController.cs
@@ -1,18 +1,44 @@
 ﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
 using HospitalTest.Infrastructure;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
 namespace HospitalTest.Controllers;
 
 [Route("api/[controller]")]
 [ApiController]
 public class DoctorController(HospitalContext ctx, IMapper mapper) : ControllerBase
 {
+    private const string _asc = "asc";
+    private Expression<Func<Doctor, object?>> GetOrderSelector(string orderProp)
+    {
+        return orderProp switch
+        {
+            "area_id" => d => d.Area == null ? 1 : d.Area.Id,
+            "specialization_id" => d => d.SpecializationId,
+            "cabinet_id" => d => d.CabinetId,
+            "fullname" => d => d.FullName, 
+            _ => d => d.Id
+        };
+}
     // GET: api/<DoctorController>
     [HttpGet]
-    public IEnumerable<string> Get()
+    public async Task<IActionResult> GetDoctors(
+        [FromQuery] string orderProp, [FromQuery] string order, 
+        [FromQuery] int page, [FromQuery] int pageSize)
     {
-        throw new NotImplementedException();
+        var orderSelector = GetOrderSelector(orderProp);
+        var doctors = ctx.Doctors.AsQueryable();
+        if(order == _asc)
+            doctors = doctors.OrderBy(orderSelector);
+        else
+            doctors = doctors.OrderByDescending(orderSelector);
+        return Ok(await doctors
+            .Skip((page - 1) * pageSize)
+            .Take(pageSize)
+            .ProjectTo<GetDoctorDto>(mapper.ConfigurationProvider)
+            .ToArrayAsync());
     }
     // GET api/<DoctorController>/5
     [HttpGet("{id}")]
---
+++ Infrastructure/Dto/GetDoctorDto.cs
@@ -0,0 +1,10 @@
+﻿namespace HospitalTest.Infrastructure;
+
+public class GetDoctorDto
+{
+    public long Id { get; set; }
+    public string FullName { get; set; } = null!;
+    public string CabinetNumber { get; set; } = null!;
+    public string SpecializationName { get; set; } = null!;
+    public string? AreaName { get; set; }
+}
